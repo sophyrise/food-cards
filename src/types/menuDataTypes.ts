0a3f0b5b0f5074af2d95ebccb844b378919364ad
Sopho Gurjishvili <123485326+sophyrise@users.noreply.github.com>
2025-09-22 22:50:19 +0400

    add menu data types and interfaces of icon and food card

---
+++ src/types/menuDataTypes.ts
@@ -0,0 +1,23 @@
+export interface IconType {
+    img: string;
+    background: string;
+}
+
+export interface MenuItem {
+    img: string;
+    title: string;
+    description: string;
+    calories: number;
+    calorieDetails: string;
+    deg: number;
+    newPrice: number;
+    oldPrice: number;
+    btnText: string;
+    icons: IconType[];
+}
+
+export type Menu = {
+    pizza: MenuItem;
+    pasta: MenuItem;
+    cake: MenuItem;
+}

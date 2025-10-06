ca13563d2ca3b9407bdc81aa3337efb870095bd3
Sopho Gurjishvili <123485326+sophyrise@users.noreply.github.com>
2025-10-06 21:37:32 +0400

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import FoodCard from "./components/FoodCard";
+import PizzaImg from "../src/images/pizza.svg";
+import PastaImg from "../src/images/pasta.svg";
+import CakeImg from "../src/images/cake.svg";
+import fireIcon from "../src/images/fireIcon.svg";
+import leafIcon from "../src/images/leafIcon.svg";
+import wheatIcon from "./images/wheatIcon.svg";
+import { menu } from "./data/menuData";
+
+const pizzaIcons = [
+  { img: fireIcon, background: "red" },
+  { img: wheatIcon, background: "orange" },
+];
+
+const pastaIcons = [
+  { img: leafIcon, background: "green" },
+  { img: fireIcon, background: "red" },
+  { img: wheatIcon, background: "orange" },
+];
+
+const cakeIcons = [{ img: wheatIcon, background: "orange" }];
+
 function App() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center bg-[#D8D8D8] gap-10">
-
+      <FoodCard {...menu.pizza} img={PizzaImg} icons={pizzaIcons} />
+      <FoodCard {...menu.pasta} img={PastaImg} icons={pastaIcons} />
+      <FoodCard {...menu.cake} img={CakeImg} icons={cakeIcons} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/types/menuDataTypes.ts
+++ src/types/menuDataTypes.ts
@@ -1,23 +1,23 @@
 export interface IconType {
-    img: string;
-    background: string;
+  img: string;
+  background: string;
 }
 
 export interface MenuItem {
-    img: string;
-    title: string;
-    description: string;
-    calories: number;
-    calorieDetails: string;
-    deg: number;
-    newPrice: number;
-    oldPrice: number;
-    btnText: string;
-    icons: IconType[];
+  img: string;
+  title: string;
+  description: string;
+  calories: number;
+  calorieDetails: string;
+  deg: number;
+  newPrice: number;
+  oldPrice: number;
+  btnText: string;
+  icons: IconType[];
 }
 
 export type Menu = {
-    pizza: MenuItem;
-    pasta: MenuItem;
-    cake: MenuItem;
-}
+  pizza: MenuItem;
+  pasta: MenuItem;
+  cake: MenuItem;
+};
